3eac29da4c78b6f7f8fa03795ad9cd4980ff38cf
geenoprx <117887625+geenoprx@users.noreply.github.com>
2026-03-28 17:17:23 +0700

    add route employee ,  event-log, leave (+department)

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import auth from './routes/auth.js'
+import employeeRouter from './routes/employee.route.js'
+import leaveRouter from './routes/leave.route.js'
+import eventLogRouter from './routes/event-log.route.js'
 
 const app = new Hono()
 
@@ -13,10 +16,15 @@ app.use('*', cors({
 }))
 
 app.route('/auth', auth)
+app.route('/employees', employeeRouter)
+app.route('/leaves', leaveRouter)
+app.route('/event-logs', eventLogRouter)
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
+app.get('/departments', async (c) => {
+  const deps = await prisma.departments.findMany({ orderBy: { name: 'asc' } })
+  return c.json({ data: deps })
+})
+
+serve({ fetch: app.fetch, port: 3000 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
